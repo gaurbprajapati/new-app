6027634c2265f96b6b44761db013255d52672f83
Gaurav Prajapati <gauravprajapati094@gmail.com>
2023-02-13 21:26:45 +0530

    app with infinite scrolle

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,59 +1,38 @@
-// import PropTypes from 'prop-types'
-import React, { Component } from 'react';
-import NewspaperIcon from '@mui/icons-material/Newspaper';
+import React from 'react'
+import { Link } from "react-router-dom";
 
-import { Link } from 'react-router-dom';
+const NavBar = () => {
 
-export class NavBar extends Component {
-//   static propTypes = {}
-
-
-  render() {
     return (
+        <div>
+            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">NewsMonkey</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
-      
-      <>
-
-      <div>
-
-
-        
-        <nav className="navbar fixed-top navbar-expand-lg bg-dark navbar-dark">
-            <div className="container-fluid">
-              <a className="navbar-brand" to="/about">NewsHunt <NewspaperIcon/></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"   aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                   <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                     <ul className="navbar-nav me-auto mb-2 mb-lg-0 navbar-expand-lg ">
-
-
-                     {/* this is all the catogory of news 
-                      */}
-                        <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/general">Home</Link></li>
-                        <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/business"> Business </Link> </li>
-                        <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/entertainment">Entertainment</Link></li>
-                        <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/general"> General </Link></li>
-                        <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/health"> Health </Link></li>
-                        <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/science" > Science </Link></li>
-                        <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/sports"> Sports </Link></li>
-                        <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/technology"> Technology</Link></li>
-                     </ul>  
-
-
-
-{/* for ii in  */}
+                        /* this is all the catogory of news
+                            */
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-
-
-
-        </nav>
+            </nav>
         </div>
-      </>
-
     )
-  }
+
 }
 
 export default NavBar
--- src/components/News.js
+++ src/components/News.js
@@ -1,97 +1,87 @@
-import React, { Component } from "react";
-import NewsItem from "./NewsItem";
-import Spinner from "./Spinner";
+import React, { useEffect, useState } from 'react'
+
+import NewsItem from './NewsItem'
+import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
+const News = (props) => {
+    const [articles, setArticles] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [page, setPage] = useState(1)
+    const [totalResults, setTotalResults] = useState(0)
 
-  static defaultProps = {
-    country : 'in',
-    pageSize : 8,
-    category:'general'
-    
-  }
-  static propTypes = {
-    country : PropTypes.string,
-    pageSize : PropTypes.number,
-    category : PropTypes.string
-  }
-
-  capitalizeFirstletter = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-
-  
-
-  constructor(props){
-    super(props);
-    this.state = {
-      articles : [],
-      loading:false,
-      page:1
+    const capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
-    document.title = `${this.capitalizeFirstletter(this.props.category) + "- NewsHunt" }`
-  }
+    const updateNews = async () => {
+        props.setProgress(10);
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+        setLoading(true)
+        let data = await fetch(url);
+        props.setProgress(30);
+        let parsedData = await data.json()
+        props.setProgress(70);
+        setArticles(parsedData.articles)
+        setTotalResults(parsedData.totalResults)
+        setLoading(false)
+        props.setProgress(100);
+    }
 
-  async updateNews(pageNo){
-    const url =  `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2630340537b44a2bbdce2fcb55d74bed&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    this.setState({
-        articles: parsedData.articles,
-        totalResults: parsedData.totalResults,
-        loading: false
-    })
-}
-
-  
-  async componentDidMount (){
-    this.updateNews();
-
-  }
-  
-  
-  handlePrevClick=  async  ()=>{
-    this.setState({ page: this.state.page - 1 });
-    this.updateNews();
-
-  }
-  
-  handleNextClick=  async  ()=>{
-    this.setState({ page: this.state.page + 1 });
-    this.updateNews();
-    
-  }
+    useEffect(() => {
+        document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
+        updateNews();
+        // eslint-disable-next-line
+    }, [])
 
 
-  
-  render() {
+    const fetchMoreData = async () => {
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`;
+        setPage(page + 1)
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        setArticles(articles.concat(parsedData.articles))
+        setTotalResults(parsedData.totalResults)
+    };
+
     return (
-      <div className="container my-3">
-      {this.state.loading && <Spinner/>}
-      {/* <h1 className="text-center">NewHunt Headlines</h1> */}
-      <h1 className="text-center" style={{ marginTop: '90px' }}>NewsHunt - top {this.capitalizeFirstletter(this.props.category)} Headlines</h1>
-        <div className="row">
-        {!this.state.loading && this.state.articles.map((element)=>{
-          return(
+        <>
+            <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+            {loading && <Spinner />}
+            <InfiniteScroll
+                dataLength={articles.length}
+                next={fetchMoreData}
+                hasMore={articles.length !== totalResults}
+                loader={<Spinner />}
+            >
+                <div className="container">
 
-           <div className="col-md-4" key={element.url} >
-            <NewsItem  title={element.title ? element.title.slice(0,44) : ""} description={ element.description ? element.description.slice(0,200): ""} imageUrl={element.urlToImage}  newsUrl={element.url} author={element.author} date={element.publishedAt}  source = {element.source.name} />
-          </div>
-          )
-          
-        })}
-        </div>
+                    <div className="row">
+                        {articles.map((element) => {
+                            return <div className="col-md-4" key={element.url}>
+                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                            </div>
+                        })}
+                    </div>
+                </div>
+            </InfiniteScroll>
+        </>
+    )
 
-        <div className="container d-flex justify-content-between">
-        <button type="button" disabled={this.state.page<=1} onClick={this.handlePrevClick} className="btn btn-dark">&larr; Previous</button>
-        <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" onClick={this.handleNextClick} className="btn btn-dark">Next &rarr;</button>
-        </div>
-      </div>
-    );
-  }
 }
 
-export default News;
+
+News.defaultProps = {
+    country: 'in',
+    pageSize: 8,
+    category: 'general',
+}
+
+News.propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+}
+
+export default News
