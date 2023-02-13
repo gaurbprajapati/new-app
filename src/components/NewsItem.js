2cd1b192b1907537c838654051c2187b264a064b
Gaurav Prajapati <gauravprajapati094@gmail.com>
2023-02-13 21:58:13 +0530

    final app

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,42 +1,30 @@
-import React, { Component } from 'react'
-import { Stack } from '@mui/material';
+import React from 'react'
 
-export class NewsItem extends Component {
-
-  const = ""
-  render() {
-
-    let {title,description , imageUrl, newsUrl , author , date , source  } = this.props;
-    // this is how expracting only date form date of  the date of publication of artical using regular expression
-    const dateStr = date.match(/(\d{1,4}([.\-/])\d{1,2}([.\-/])\d{1,4})/g);
-    const Unknown = source;
-    return (
-      <>
-      <div className="my-3">
-
-        <div className="card" >
-        
-          <img className="card-img-top" rel="noreferrer" src={!imageUrl ? "https://static.euronews.com/articles/stories/06/91/09/58/945x531_screenshot_1200_15_6910958.jpg" : imageUrl } alt="someThing loading....."/>
-              <div className="card-body">
-                  <h5 className="card-title">{title}</h5>
-                  <Stack sx={{ ml:{lg:'900px' , xs:'500px'} }}>
-                  <span className="position-absolute top-0 start-200 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'  , marginRight:'70px'}}> {source} </span>
-                  </Stack>
-                  <p className="card-text">{description}</p>
-                  {/* <p className="card-text"><small className="text-muted">By {!author ? "Unknown" :author } on {new Date (date).toGMTString} </small></p> */}
-                  <p className="card-text"><small className="text-muted">By {!author? Unknown: author} on {dateStr}</small></p>
-                  <a href={newsUrl} rel="noreferrer" target='_blank' className="btn btn-sm btn-dark">Read More </a>
-              </div>
-        </div>
-
-      </div>
-
-
-
-
-      </>
-    )
-  }
+const NewsItem = (props)=> {
+        let { title, description, imageUrl, newsUrl, author, date, source } = props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }> 
+                        <span className="badge rounded-pill bg-danger"> {source} </span>
+                    </div>
+                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}  </h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+     
 }
 
 export default NewsItem
